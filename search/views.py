e98da6edb5a5c4470a7c281b58d02b62af26729c
mikbhub <mikolaj.babiak@gmail.com>
2017-06-12 17:47:35 +0200

    removed unused code

--- search/views.py
+++ search/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import HttpResponse, reverse, render
-from django.views.decorators.csrf import csrf_exempt
-from django.views import View, generic
-from django.utils.decorators import method_decorator
+from django.shortcuts import render
+from django.views import View
 from rezerwacje.models import Auditorium
 
 
@@ -13,38 +11,19 @@ class SearchAvaliableAuditoriums(View):
 
 class ShowSearchResults(View):
 
-    def get(self, request):
-
+    def database_query(self, request):
         # build database query using GET params:
         filter_params = dict()
         exclude_params = dict()
-
         if 'date' in request.GET and request.GET['date'] != '':
             exclude_params['reservations__date'] = request.GET['date']
-
         if 'capacity' in request.GET and request.GET['capacity'] != '':
             filter_params['capacity__gte'] = request.GET['capacity']
-
         if 'projector' in request.GET and request.GET['projector'] != '':
             filter_params['projector'] = request.GET['projector']
+        return Auditorium.objects.exclude(**exclude_params).filter(**filter_params).order_by('capacity')
 
-        auditoriums = Auditorium.objects.exclude(**exclude_params).filter(**filter_params).order_by('capacity')
-
+    def get(self, request):
+        auditoriums = self.database_query(request)
         context = {'auditoriums': auditoriums}
-
         return render(request, 'search/results.html', context)
-
-        # if len(auditoriums) == 0:
-        #     return HttpResponse('Brak wolnych sal dla podanych kryteriów wyszukiwania')
-        # else:
-        #     auditoriums = '<br>'.join('{name}, Capacity: {capacity}. {reservation_link}'.format(
-        #             name=auditorium.name,
-        #             capacity=auditorium.capacity,
-        #             reservation_link='<a href="{}">Reserve</a>'.format(reverse('reserve', kwargs={'id_':auditorium.pk}))
-        #         ) for auditorium in auditoriums)
-        #     return HttpResponse(
-        #         f"""
-        #         Available auditoriums:<br>
-        #         {auditoriums}
-        #         """
-        #     )
